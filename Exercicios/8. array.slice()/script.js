e7dfcff46bf43e58d60bc1b595eba380377ad52e
Vivian Caldas <Vivian_Caldas@Dell.com>
2022-05-06 14:12:59 -0300

    inclusão de exercícios de aula e exercícios de casa.

---
+++ Exercicios/8. array.slice()/script.js
@@ -0,0 +1,18 @@
+const listagemDeFrutas = [ "Uva", "Banana", "Manga", "Cajá", "Pinha", "Maçã", "Melão"];
+
+/* const selectedItens = listagemDeFrutas.slice(1,4);
+
+console.log(selectedItens); */
+
+const usuarios = [
+    {user:234, name: 'Marcia', idade:40 },
+    {user:235, name: 'Lorena', idade:20 },
+    {user:236, name: 'Patricia', idade:24 },
+    {user:237, name: 'Mariana', idade:15 },
+    {user:238, name: 'Isis', idade:34 },
+    {user:239, name: 'Pietra', idade:23 }
+]
+
+const selectedItens = usuarios.slice(0,3);
+
+console.log(selectedItens)
