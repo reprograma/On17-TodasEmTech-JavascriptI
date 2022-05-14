806613613e6468c204dba107efb32588ebb1cd76
Juliana <juliana@MacBook-Pro-de-Juliana.local>
2022-05-14 18:18:30 -0300

    juliana-albuquerque

--- Exercício de Casa/Exercício 3/script.js
+++ Exercício de Casa/Exercício 3/script.js
@@ -1,4 +1,12 @@
-const scores = [1, 2, 2, 2, 5, 8, 9, 10]
-const getOccurrences = (array, value) => 
-    array.reduce((acc, item) => value === item, ? acc + 1 : acc 0)
-console.log(getOccurrences(scores, 2));
+const scores = [1, 2, 2, 2, 5, 8, 9, 10];
+  const numero = 2;
+  
+  function frequenciaDoNumero(scores, numeroEscolhido) {
+    const arrayNumeroSelecionado = scores.filter((numero) => {
+      if (numero == numeroEscolhido) return numero;
+    });
+    console.log(arrayNumeroSelecionado);
+    return `A frequencia do ${numeroEscolhido} é ${arrayNumeroSelecionado.length}`;
+  }
+  
+  console.log(frequenciaDoNumero(scores, numero));
