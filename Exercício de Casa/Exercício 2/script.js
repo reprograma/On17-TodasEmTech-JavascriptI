4f11af0dd28d86944f199ecd930bc34aef3b3625
Juliana Melo <juliana.melos@outlook.com>
2022-05-14 15:11:38 -0300

    atividades 1, 2 e 3 finalizadas

---
+++ Exercício de Casa/Exercício 2/script.js
@@ -0,0 +1,8 @@
+const osNumeros = [-1, 0, 1, 2, 3, 4, 6.4, 5, 6, 7, 8, 9, 10, -40, 4.7];
+    console.log(osNumeros.length);
+
+var numeroMenor = osNumeros.slice (13,14);
+    console.log(numeroMenor);
+
+var numeroMaior = osNumeros.slice(12,13);
+    console.log(numeroMaior);
---
+++ Exercício de Casa/Exercício 3/script.js
@@ -0,0 +1,10 @@
+    const anotherArray = [ 2, 3, 4, 5, 4, 7, 10, 22, 43, 4, 4, 20, 4 ];
+      console.log(anotherArray.length)
+
+    let frequenciaNumero = anotherArray.filter(function(anotherArray) {
+      return anotherArray == 4
+    });
+
+    console.log(frequenciaNumero);
+
+      
