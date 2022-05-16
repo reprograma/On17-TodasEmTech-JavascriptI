712c23b68ee4743bd406f1088c02664ac69f8d17
Juliana Melo <juliana.melos@outlook.com>
2022-05-16 10:17:27 -0300

    resposta da atividade 3 reformulada

--- Exercício de Casa/Exercício 3/script.js
+++ Exercício de Casa/Exercício 3/script.js
@@ -1,10 +1,9 @@
     const anotherArray = [ 2, 3, 4, 5, 4, 7, 10, 22, 43, 4, 4, 20, 4 ];
       console.log(anotherArray.length)
 
-    let frequenciaNumero = anotherArray.filter(function(anotherArray) {
-      return anotherArray == 4
-    });
-
-    console.log(frequenciaNumero);
-
+    let frequenciaNumero = anotherArray.filter  (frequencia => (frequencia) === 4);
+      console.log(frequenciaNumero);
       
+   
+
+   
