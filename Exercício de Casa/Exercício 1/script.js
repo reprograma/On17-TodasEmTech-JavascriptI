221e1f6470e80a9aceee222869bcdb20a5e14a58
andressa05gomes <andressa05gomes@gmail.com>
2022-05-19 08:59:23 -0300

    https://github.com/andressa05gomes/On17-TodasEmTech-JavascriptI.git

--- Exercício de Casa/Exercício 1/script.js
+++ Exercício de Casa/Exercício 1/script.js
@@ -48,3 +48,32 @@ const books = [
         published: "2014-04-07T00:00:00.000Z",
     }
 ]
+
+
+function mapeandoLivros(objeto) {
+  const livrosMapeados = objeto.map(({ title, author, published }) => {
+    return { title, author, published };
+  });
+  console.log(livrosMapeados);
+}
+mapeandoLivros(books);
+
+function removendoEsubstituindo(objeto) {
+  objeto.splice(5, 1, {
+    isbn: "9781788623872",
+    title: "Learning JavaScript Data Structures and Algorithms",
+    author: "Loiane Groner",
+    published: "2018-04-26T00:00:00.000Z",
+  });
+  return console.log(objeto);
+}
+removendoEsubstituindo(books);
+
+function formatandoData(objeto) {
+  objeto.forEach(({ published }) => {
+    data = published.replace("T00:00:00.000Z", "");
+    dataPtBr = data.split("-").reverse().join("/");
+    return console.log(dataPtBr);
+  });
+}
+formatandoData(books);
---
+++ Exercício de Casa/Exercício 3/script.js
@@ -0,0 +1,5 @@
+var sequence = [1, 1, 1, 1, 2];
+for (var i = 0; i < sequence.length; i++) {
+  console.log(sequence[i]);
+}
+
