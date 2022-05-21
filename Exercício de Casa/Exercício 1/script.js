a0b36c0a4fcdb9110a85a0058f81be4a15afc019
Catharine Lopes <catharine.augusto@gmail.com>
2022-05-21 16:37:02 -0300

    subindobranch

--- Exercício de Casa/Exercício 1/script.js
+++ Exercício de Casa/Exercício 1/script.js
@@ -47,4 +47,34 @@ const books = [
         author: "Glenn Block, et al.",
         published: "2014-04-07T00:00:00.000Z",
     }
+    
 ]
+
+function organizandoLivros(objeto) {
+    const livrosOrganizados = objeto.map(({ title, author, published }) => {
+      return { title, author, published };
+    });
+    console.log(livrosOrganizados);
+  }
+  mapeandoLivros(books);
+  
+  function removendoEsubstituindo(objeto) {
+    objeto.splice(5, 1, {
+      isbn: "9781788623872",
+      title: "Learning JavaScript Data Structures and Algorithms",
+      author: "Loiane Groner",
+      published: "2018-04-26T00:00:00.000Z",
+    });
+    return console.log(objeto);
+  }
+  removendoEsubstituindo(books);
+  
+  function formatandoData(objeto) {
+    objeto.forEach(({ published }) => {
+      data = published.replace("T00:00:00.000Z", "");
+      dataPtBr = data.split("-").reverse().join("/");
+      return console.log(dataPtBr);
+    });
+  }
+  formatandoData(books);
+  
