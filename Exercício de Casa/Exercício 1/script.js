31e12b2ea6e5aa3ce70dedeaf43b2c59895220bc
Luciane Silva <ltsluciane@icloud.com>
2022-07-06 13:07:43 -0300

    exercio de casa

--- Exercício de Casa/Exercício 1/script.js
+++ Exercício de Casa/Exercício 1/script.js
@@ -48,3 +48,19 @@ const books = [
         published: "2014-04-07T00:00:00.000Z",
     }
 ]
+
+books.splice(1,5, {isbn: "9781788623872",
+title: "Learning JavaScript Data Structures and Algorithms",
+author: "Loiane Groner",
+published: "2018-04-26T00:00:00.000Z",});
+
+console.log(books);
+
+function formatandoData(objeto) {
+    objeto.forEach(({ published }) => {
+      data = published.replace("T00:00:00.000Z", "");
+      dataPtBr = data.split("-").reverse().join("/");
+      return console.log(dataPtBr);
+    });
+  }
+  formatandoData(books);
