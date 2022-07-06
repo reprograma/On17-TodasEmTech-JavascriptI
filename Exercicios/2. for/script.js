276de7fdbb5e39607103353ee364fce005e03c89
Luciane Silva <ltsluciane@icloud.com>
2022-07-06 12:52:33 -0300

    exercicio de casa

--- Exercicios/2. for/script.js
+++ Exercicios/2. for/script.js
@@ -32,24 +32,46 @@ console.log(sobrinhosDaVivian[1].castrado); */
 
 const listagemDeFrutas = ["Uva", "Banana", "Manga", "Cajá", "Pinha", "Maçã", "Melão"];
 
-for(let fruta = 0; fruta < listagemDeFrutas.length; fruta++) {
-   console.log(`A minha fruta é ${listagemDeFrutas[fruta]}`);
+for (let fruta = 0; fruta < listagemDeFrutas.length; fruta++) {
+    console.log(`A minha fruta é ${listagemDeFrutas[fruta]}`);
 }
 
-const usuarios = [
-    {user: 234, name: 'Marcia', idade: 40 },
-    {user: 235, name: 'Lorena', idade: 20 },
-    {user: 236, name: 'Patricia', idade: 24 },
-    {user: 237, name: 'Mariana', idade: 15 },
-    {user: 238, name: 'Isis', idade: 34 },
-    {user: 239, name: 'Pietra', idade: 23 }
+const usuarios = [{
+        user: 234,
+        name: 'Marcia',
+        idade: 40
+    },
+    {
+        user: 235,
+        name: 'Lorena',
+        idade: 20
+    },
+    {
+        user: 236,
+        name: 'Patricia',
+        idade: 24
+    },
+    {
+        user: 237,
+        name: 'Mariana',
+        idade: 15
+    },
+    {
+        user: 238,
+        name: 'Isis',
+        idade: 34
+    },
+    {
+        user: 239,
+        name: 'Pietra',
+        idade: 23
+    }
 ]
 
-for(let contador = 0; contador < usuarios.length; contador++) {
+for (let contador = 0; contador < usuarios.length; contador++) {
     if (usuarios[contador].user === 235) {
         console.log(usuarios[contador].name);
     } else {
         console.log(usuarios[contador].name, usuarios[contador].idade)
     }
 }
-
